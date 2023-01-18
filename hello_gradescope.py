ae7e57469acd6cd2538bbd7bc5990c95b7845b03
suhrobh1 <suhrobhasanov1@gmail.com>
2023-01-17 21:52:24 -0800

    Initial

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Suhrob Hasanov
+# OSU Email: hasanovs@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 01/18
+# Description: Assignment 0 to gradescope
+
+
+def gradescope_test():
+    return 42
